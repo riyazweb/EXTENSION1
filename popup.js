f2b33073768e2c0bd22a78cd8edb5565b95ef465
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-02-28 20:52:10 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("extract-btn").addEventListener("click", extractImageURL);
+  });
+  
+  function extractImageURL() {
+    chrome.tabs.executeScript({
+      code: 'var previewDiv = document.querySelector("div.preview"); previewDiv.style.backgroundImage.match(/url\\((.*?)\\)/)[1];'
+    }, function(results) {
+      var imageURL = results[0];
+      if (imageURL) {
+        imageURL = imageURL.replace(/['"]/g, "");
+        imageURL = imageURL.replace(/amp;/g, "");
+        if (imageURL.startsWith("//")) {
+          imageURL = "http:" + imageURL;
+        }
+        copyToClipboard(imageURL);
+        chrome.tabs.create({ url: imageURL });
+      } else {
+        alert("No image URL found.");
+      }
+    });
+  }
+  
+  function copyToClipboard(text) {
+    var textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  }
+  
